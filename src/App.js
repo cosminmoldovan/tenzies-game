e2537722177939b1216430519e87cd891f4a8df9
Cosmin Moldovan <cosmin.moldovan@protonmail.com>
2022-06-06 17:06:39 +0300

    update rollDice to keep holded dice

--- src/App.js
+++ src/App.js
@@ -38,9 +38,11 @@ function App() {
     ))
   }
   const diceElements = dice.map(die => <Die key={die.id} value={die.value} isHeld={die.isHeld} holdDice={()=>holdDice(die.id)}/>);
-  
+
   return (
    <main>
+     <h1 className="title">Tenzies</h1>
+     <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
      <div className='dice-container'>
        {diceElements}
      </div>
